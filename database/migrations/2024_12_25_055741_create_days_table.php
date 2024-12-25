d21ec2d39ccc191e8fff44c0e9034bd81c222ec7
Odiljon <odilorg@gmail.com>
2024-12-25 10:58:04 +0500

    start

---
+++ database/migrations/2024_12_25_055741_create_days_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('days', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('tour_id')->constrained()->onDelete('cascade'); // A day belongs to a tour
+            $table->integer('day_number'); // Day 1, Day 2, etc.
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('days');
+    }
+};
