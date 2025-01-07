c3ff2e3dbdfb2078710d5769a16d2dc993b158f6
odil <odilorg@gmail.com>
2025-01-07 19:59:01 +0500

    access to admin panel

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+
+
+class User extends Authenticatable implements FilamentUser
 {
+    // ...
+ 
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return self::where('email', $this->email)->exists(); 
+        }
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
