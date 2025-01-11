26f189e9379d8ac363e34946b86d5807edd8e95a
odil <odilorg@gmail.com>
2025-01-11 10:14:22 +0500

    hotel done

--- app/Filament/Resources/HotelResource.php
+++ app/Filament/Resources/HotelResource.php
@@ -9,8 +9,11 @@ use Filament\Forms\Form;
 use Filament\Tables\Table;
 use Filament\Resources\Resource;
 use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Fieldset;
 use Filament\Forms\Components\Repeater;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Forms\Components\FileUpload;
 use Filament\Tables\Columns\SelectColumn;
 use Illuminate\Database\Eloquent\Builder;
@@ -31,94 +34,144 @@ class HotelResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('address')
-                    ->maxLength(255),
-                Select::make('city_id')
-                    ->relationship('city', 'name')
-                    ->required()
-                    ->preload()
-                    ->createOptionForm([
-                        Forms\Components\TextInput::make('name')
-                        ->required()
-                        ->maxLength(255),
-                    Forms\Components\Textarea::make('description')
-                        ->maxLength(555)
-                        ->default(null),
-                    Forms\Components\FileUpload::make('images')
-                    ->multiple()
-                    ->image()
-                        ->columnSpanFull(),
-                    ]),                    
-
-                Select::make('type')
-                    ->label('Category')
-                    ->options([
-                        'bed_breakfast' => 'B&B',
-                        '3_star' => '3 Star',
-                        '4_star' => '4 Star',
-                        '5_star' => '5 Star',
-                    ])
-                    // ->default('bus')  // If you want a default value
-                    ->required(),
-
-
-                Repeater::make('rooms')
-                    ->relationship()
+                Section::make('Инфо Гостиницы')
+                    ->description('Введите информацию о гостинице')
                     ->schema([
-
-                        Forms\Components\select::make('room_type_id')
-                            ->relationship('roomType', 'type')
-                            ->required()
-                            ->preload()
-                            ->createOptionForm([
-                                Forms\Components\TextInput::make('type')
-                                    ->required(),
-                            ]),
                         Forms\Components\TextInput::make('name')
+                        ->label('Название гостиницы')
                             ->required()
                             ->maxLength(255),
-                        Forms\Components\Textarea::make('description')
-                            ->columnSpanFull(),
 
-
-                        Select::make('amenities')
-                            ->multiple()
+                        Forms\Components\TextInput::make('phone')
+                        ->label('Телефон')
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('email')
+                        ->label('Email')    
+                            ->required()
+                            ->maxLength(255),
+                        Forms\Components\TextInput::make('address')
+                        ->label('Адрес')
+                            ->maxLength(255),
+                        Select::make('city_id')
+                            ->relationship('city', 'name')
+                            ->label('Город расположения')
+                            ->required()
                             ->preload()
-                            ->searchable()
-                            ->relationship(titleAttribute: 'name')
                             ->createOptionForm([
                                 Forms\Components\TextInput::make('name')
-                                    ->required(),
-
+                                    ->required()
+                                    ->maxLength(255),
+                                Forms\Components\Textarea::make('description')
+                                    ->maxLength(555)
+                                    ->default(null),
+                                Forms\Components\FileUpload::make('images')
+                                    ->multiple()
+                                    ->image()
+                                    ->columnSpanFull(),
                             ]),
-                        Forms\Components\TextInput::make('cost_per_night')
-                            ->required()
-                            ->numeric()
-                            ->default(0.00),
-                        FileUpload::make('images')
-                            ->multiple(),
+                        Select::make('type')
+                            ->label('Категория гостиницы')
+                            ->options([
+                                'bed_breakfast' => 'B&B',
+                                '3_star' => '3 Star',
+                                '4_star' => '4 Star',
+                                '5_star' => '5 Star',
+                            ])
+                            // ->default('bus')  // If you want a default value
+                            ->required(),
+                        Forms\Components\Textarea::make('description')
+                            ->label('Описание гостиницы')
+                            ->maxLength(555)
+                            ->default(null),
+                        Forms\Components\FileUpload::make('images')
+                            ->label('Фотографии гостиницы')
+                            ->multiple()
+                            ->image(),
+
+                    ])->columns(2),
+
+                    Section::make('Номера гостиницы')
+                    ->description('Введите типы и стоимость номеров')
+                    ->schema([
+                        Repeater::make('rooms')
+                        ->label('Номера')
+                        ->relationship()
+                        ->schema([
+    
+                            Forms\Components\select::make('room_type_id')
+                                ->relationship('roomType', 'type')
+                                ->label('Тип номера или создать новый нажми плюсик в конце')
+                                ->required()
+                                ->preload()
+                                ->createOptionForm([
+                                    Forms\Components\TextInput::make('type')
+                                        ->required(),
+                                ]),
+                            // Forms\Components\TextInput::make('name')
+                            //     ->required()
+                            //     ->maxLength(255),
+                            
+    
+    
+                            Select::make('amenities')
+                            ->label('Удобства в номере или создать новый нажми плюсик в конце')
+                                ->multiple()
+                                ->preload()
+                                ->searchable()
+                                ->relationship(titleAttribute: 'name')
+                                ->createOptionForm([
+                                    Forms\Components\TextInput::make('name')
+                                        ->required(),
+    
+                                ]),
+                            Forms\Components\TextInput::make('cost_per_night')
+                                ->label('Стоимость за ночь')
+                                ->required()
+                                ->numeric(),
+                               
+                                Forms\Components\Textarea::make('description')
+                                ->label('Описание номера')
+                                ->columnSpanFull(),    
+                            FileUpload::make('images')
+                            ->label('Фотографии номера')
+                                ->multiple()
+                                ->columnSpanFull(),   
+                        ])
                     ])
 
 
-            ]);
+                   
+                        
+                   
+
+
+
+                
+
+            ])->columns(2);
     }
 
     public static function table(Table $table): Table
     {
         return $table
             ->columns([
-                TextColumn::make('name'),
-                TextColumn::make('address'),
-                SelectColumn::make('category')
-                    ->options([
-                        'bed_breakfast' => 'B&B',
-                        '3_star' => '3 Star',
-                        '4_star' => '4 Star',
-                        '5_star' => '5 Star',
-                    ]),
+                TextColumn::make('name')
+                    
+                    ->searchable()
+                    ->label('Название'),
+                TextColumn::make('city.name')
+                    ->searchable()
+                    ->label('Город'),
+                TextColumn::make('type')
+                    ->label('Категория'),
+                TextColumn::make('phone')    
+                    ->label('Телефон'),
+                TextColumn::make('email')    
+                    ->label('Email'),
+                    ImageColumn::make('images')
+                    ->circular()
+                    ->stacked()     
             ])
             ->filters([
                 //
