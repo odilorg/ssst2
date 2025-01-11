38682c86ffdc50fbc9a75aa808038af1de4f7815
Odiljon <odilorg@gmail.com>
2025-01-11 17:40:45 +0500

    edit

--- app/Filament/Resources/TransportResource/Pages/EditTransport.php
+++ app/Filament/Resources/TransportResource/Pages/EditTransport.php
@@ -16,4 +16,8 @@ class EditTransport extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
