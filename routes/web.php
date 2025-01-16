75fbfd3fa422a23cde1970930c3dd8ad14034a7f
Odiljon <odilorg@gmail.com>
2025-01-16 08:58:57 +0500

    web cleaned

--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 use Barryvdh\DomPDF\Facade\Pdf;
 
-Route::get('/test-pdf', function () {
-    // Example data for the PDF
-    $data = [
-        'title' => 'Invoice',
-        'date' => date('m/d/Y'),
-        'invoice_number' => 'INV-12345',
-        'items' => [
-            ['description' => 'Item 1', 'quantity' => 2, 'price' => 50],
-            ['description' => 'Item 2', 'quantity' => 1, 'price' => 30],
-        ],
-    ];
 
-    // Load the view and generate the PDF
-    $pdf = Pdf::loadView('pdf.invoice', $data);
-
-    // Return the PDF as a download
-    return $pdf->download('invoice.pdf');
-});
 
