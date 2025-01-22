327ca470627745f363e6ee82f1ac32451d40c517
odilorg <odilorg@gmail.com>
2025-01-22 14:27:03 +0500

    fixed migration

--- database/migrations/2025_01_22_081302_create_tour_day_hotels_table.php
+++ database/migrations/2025_01_22_081302_create_tour_day_hotels_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('tour_day_hotels', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->string(column: 'type')->nullable()->after('hotel_id'); // Add type column
+            $table->string(column: 'type')->nullable(); // Add type column
             $table->foreignId('tour_day_id')->constrained('tour_days')->cascadeOnDelete();
             $table->foreignId('hotel_id')->constrained('hotels')->cascadeOnDelete();
 
